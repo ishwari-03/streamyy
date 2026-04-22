73b71d5e8473fa30d18d93d5d72cf4701a37f0d0
ishwari-03 <ishwaried101@gmail.com>
2026-04-22 20:29:52 +0530

    Fix CORS configuration for local and production

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,9 +17,11 @@ const PORT=process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(cors({
-    origin: true,
+    origin: process.env.NODE_ENV === "production" 
+        ? process.env.FRONTEND_URL 
+        : "http://localhost:5173",
     credentials: true,
-     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
